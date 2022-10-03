4f762f903c641284ad6de58b8735e27e2de46b58
maxielsezar <elsezarmaxi@gmail.com>
2022-10-03 01:38:28 -0300

    se añadio los iconos de los contactos con la funcion para poner la imagen a la pantalla del local

--- app/src/main/java/com/desarrolloswesquel/todocomida/controladores/adapters/ContactoAdapter.kt
+++ app/src/main/java/com/desarrolloswesquel/todocomida/controladores/adapters/ContactoAdapter.kt
@@ -3,6 +3,7 @@ package com.desarrolloswesquel.todocomida.controladores.adapters
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.desarrolloswesquel.todocomida.R
@@ -11,25 +12,39 @@ import com.desarrolloswesquel.todocomida.modelo.Contacto
 class ContactoAdapter(
     private var ListaContacto: MutableList<Contacto>
 
-): RecyclerView.Adapter<ContactoAdapter.contactoViewHolder>() {
+): RecyclerView.Adapter<ContactoAdapter.ContactoViewHolder>() {
 
-    class contactoViewHolder(view: View): RecyclerView.ViewHolder(view) {
-        val nombreContacto: TextView = view.findViewById(R.id.ItemTipoContacto)
+    class ContactoViewHolder(view: View): RecyclerView.ViewHolder(view) {
         val linkContacto: TextView = view.findViewById(R.id.ItemLinkContacto)
+        val imagenContacto: ImageView=view.findViewById(R.id.ItemContactoImagen)
 
     }
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): contactoViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactoViewHolder {
         val adapterLayout = LayoutInflater.from(parent.context)
             .inflate(R.layout.item_contacto, parent, false)
-        return contactoViewHolder(adapterLayout)
+        return ContactoViewHolder(adapterLayout)
     }
 
-    override fun onBindViewHolder(holder: contactoViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: ContactoViewHolder, position: Int) {
         val item = ListaContacto[position]
-        holder.nombreContacto.text = item.TipoContacto.name
         holder.linkContacto.text = item.InfoContacto
+        holder.imagenContacto.setImageResource(cargarImagen(item.TipoContacto.name))
     }
 
     override fun getItemCount() = ListaContacto.size
+
+
+    private fun cargarImagen(TipoContacto: String): Int{
+        val drawableResource = when (TipoContacto) {
+            "NumTelefonoFijo" -> R.drawable.telefono
+            "NumTelefonoMovil"-> R.drawable.celu
+            "LinkFacebook"-> R.drawable.facebook
+            "LinkWhatsapp"-> R.drawable.whatsapp
+            "LinkInstagram"-> R.drawable.instagram
+            else -> R.drawable.otros
+        }
+        return drawableResource
+    }
+
 
 }
